b8c7fee533ae51b9b15eb840f2da7b5d85419a1a
Austin Osorio <osorio_austin@yahoo.com>
2020-01-12 00:30:02 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -29,6 +29,8 @@ def directors_totals(nds)
 
   while dir_index < nds.length do
 
+    name = dir_index[:name]
+
     result[dir_index[:name]] = gross_for_director(nds[dir_index])
     dir_index += 1
 
